c17240e49aa7f91dbada678ccb4a1d316fd870ee
Peter Buelow <peter.buelow@lifefitness.com>
2018-05-13 08:44:04 -0500

    Lowering light level again

--- src/puck_light.ino
+++ src/puck_light.ino
@@ -2,7 +2,7 @@
 #define OFF_BUTTON          D5
 #define MOTION_DETECT       D2
 #define LIGHT_DETECT        A1
-#define VERSION             44
+#define VERSION             45
 
 #define FIVE_MINUTES        (1000 * 60 * 5)
 #define TWENTY_SECONDS      (1000 * 20)
@@ -18,7 +18,7 @@ unsigned long g_detectRemain;
 
 bool isDark()
 {
-    if (g_lux < 150)
+    if (g_lux < 100)
         return true;
 
     return false;
@@ -71,10 +71,6 @@ void setup()
     Particle.function("Off", turnLightsOff);
     Particle.variable("lux", g_lux);
     Particle.variable("version", g_version);
-    Particle.variable("motion", g_motionDetected);
-    Particle.variable("millis", g_millis);
-    Particle.variable("lockout", g_detectRemain);
-    Particle.variable("lights", g_lightsOn);
 
     g_lightsOn = false;
     g_timeOut = 0;
